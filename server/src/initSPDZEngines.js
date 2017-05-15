ef1d87ba5db30e1e370b465789abfeb967b5d740
Jonathan Evans <jon.evans@bristol.ac.uk>
2017-05-15 16:00:13 +0100

    Retry voter init at startup, to support nginx proxies not running.

--- server/src/initSPDZEngines.js
+++ server/src/initSPDZEngines.js
@@ -27,19 +27,70 @@ if (talkIds.length > 40) {
   talkIds = talkIds.concat((new Array(40 - talkIds.length)).fill(0))
 }
 
+/**
+ * A function to run a promise, if succeeds then resolve otherwise rerun promise in interval.
+ * @return Promise which resolves promiseToRetry (no return) or rejects with error.
+ */
+const retryWithInterval = (promiseToRetry, retryIntervalSecs, maxAttemptCount) => {
+  return new Promise(function (resolve, reject) {
+    let connectCount = 1;
+    promiseToRetry()
+      .then(() => {
+        logger.info(`Succeeded on attempt ${connectCount}`)
+        resolve()
+      })
+      .catch((err) => {
+        logger.info(`Failed on attempt ${connectCount}, will retry in ${retryIntervalSecs} secs.`)
+        logger.debug(`${err}.`)
+
+        connectIntervalId = setInterval(() => {
+          connectCount += 1
+          promiseToRetry()
+            .then(() => {
+              logger.info(`Succeeded on attempt ${connectCount}`)              
+              clearInterval(connectIntervalId)
+              resolve()
+            })
+            .catch((err) => {
+              if (connectCount >= maxAttemptCount) {
+                logger.info(`Failed on attempt ${connectCount}, no more retries.`)
+                clearInterval(connectIntervalId)
+                reject(err)
+              } else {
+                logger.info(`Failed on attempt ${connectCount}, will retry in ${retryIntervalSecs} secs.`)
+                logger.debug(`${err}.`)
+              }
+            })
+        }, retryIntervalSecs * 1000)
+
+      })
+    })
+  }
+
+/**
+ * Create a function to run spdz connect to proxies.
+ * @return Function which when run, returns promise which will resolve if connection OK,
+ *         or reject with error reason. 
+ */
+const proxyConnect = (spdzProxyList, spdzApiRoot, dhPublicKey) => {
+  return () => {
+    return spdzGuiLib.connectToProxies(spdzProxyList.map(spdzProxy => spdzProxy.url),
+      spdzApiRoot, dhPublicKey)
+      .then((values) => {
+        if (spdzGuiLib.allProxiesConnected(values)) {
+          return
+        }
+        else {
+          return Promise.reject(
+            new Error('Unable to connect to all Spdz Proxy Servers ' + JSON.stringify(values)))
+        }
+      })
+  }
+}
 
 const initSPDZ = (spdzProxyList, spdzApiRoot, dhPublicKey) => {
-  return spdzGuiLib.connectToProxies(spdzProxyList.map(spdzProxy => spdzProxy.url),
-                          spdzApiRoot, dhPublicKey)
-    .then((values) => {
-      if (spdzGuiLib.allProxiesConnected(values)) {
-        return 
-      }
-      else {
-        return Promise.reject(
-          new Error('Unable to connect to all Spdz Proxy Servers ' + JSON.stringify(values)))
-      }
-    })
+  logger.info("Connecting to SPDZ proxies.")
+  return retryWithInterval( proxyConnect(spdzProxyList, spdzApiRoot, dhPublicKey), 15, 4)
     .then(() => {
       logger.info("Sending voter ids to SPDZ Engines.")
       return spdzGuiLib.sendInputsWithShares(voterIds, true, spdzProxyList, spdzApiRoot, dhPublicKey, 1000)
